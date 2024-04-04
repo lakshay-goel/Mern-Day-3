59432960b7c0aa17e4108a35ebf2cc1303383df6
Lakshay Goel <102369062+lakshay-goel@users.noreply.github.com>
2024-04-04 19:32:00 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,155 @@
+// var message = "hello world 1";
+// var age = 10;
+// age = "Done";
+// console.log("hello ");
+
+//loosely tuped
+//strictly tuped
+
+//statically typed
+//dynamically typedss
+
+
+//FUNCTION
+// function env(){
+//     console.log(message)
+
+//     var message = "hello";
+
+//     console.log(message)
+
+//     var message = "else";
+
+//     console.log(message)
+// }
+
+//FUNCTION
+// function env(){
+//     for(var i=0; i<5; i++){
+//         console.log('--',i);
+//     }
+//     console.log(i);
+// }
+// env();
+
+//LET
+// let message = "hello";
+// console.log(message);
+
+// //CONST
+// const message = "hello";
+// console.log(message);
+
+//FOR POPUP MESSAGE
+// alert("ERROR")
+
+// message = "else";
+// console.log(message);
+
+//FOR CHECKING TYPE OF VARIABLE
+// let age="abcd";
+// age = 10;
+// console.log(typeof(age));
+
+//ARRAY
+// let arr=['a','b','c','d'];
+// arr[0]="abcd";
+// console.log(arr);
+
+// let str="12";
+// let age = 20;
+// let ans = age+str;
+// console.log(ans);
+
+// let str = "12";
+// let str1 = 10.3;
+// let age1 = parseInt(str1);
+// let age = Number(str);
+// console.log(age);
+// console.log(age1);
+
+// let a = "12";
+// let b = 12;
+// if(a==b){
+//     console.log("yes");
+// }
+// else{
+//     console.log("no");
+// }
+// //TRIPLE === COMPARE VALUE, DATA TYPE, ADRESS 
+// if(a===b){
+//     console.log("yes");
+// }
+// else{
+//     console.log("no");
+// }
+
+// const str="khan";
+// const usrname = "my name is: ";
+// const ans = ${usrname}${str};
+// console.log(ans);
+
+// let a = 10;
+// let b = 12;
+// function SubmitEvent(a,b){
+//     if(a && b){
+//         console.log(a+b);
+//     }
+//     console.log(a+b);
+// }
+// sum(10);
+
+
+
+//************************function declaration**************************
+
+// function print(){
+//     console.log('---');
+// }
+
+// //function assingment(named)
+
+// const a =function(){
+//     console.log('---');
+// }
+
+// //function assingment(annonymous)
+
+// const b =function(){
+//     console.log('---');
+// }
+
+// //Arrow function assingment
+
+// const c =function(){
+//     console.log('---');
+// }
+
+// print();
+// a();
+// b();
+// c();
+
+// let a=2;
+// switch(a){
+//     case 1:{
+//         console.log('one');
+//         break;
+//     }
+//     case 2:{
+//         console.log('two');
+//         break;
+//     }
+//     case 3:{
+//         console.log('three');
+//         break;
+//     }
+//     default:{
+//         console.log('not 1,2,3')
+//     }
+    
+// }
+
+const obj = new Object();
+obj[12]="twelve";
+console.log('obj')
